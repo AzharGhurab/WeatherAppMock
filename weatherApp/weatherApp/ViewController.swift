05baaa020f2768c6632cf73a409e98b4d67c5ce0
AzharGhurab <aghurab@innovationteam.com>
2026-03-04 14:01:10 +0300

    init setup

---
+++ weatherApp/weatherApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  weatherApp
+//
+//  Created by Azhar Ghurab on 15/09/1447 AH.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ weatherApp/weatherAppTests/weatherAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  weatherAppTests.swift
+//  weatherAppTests
+//
+//  Created by Azhar Ghurab on 15/09/1447 AH.
+//
+
+import Testing
+@testable import weatherApp
+
+struct weatherAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
