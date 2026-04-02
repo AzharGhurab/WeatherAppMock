5ad39d323f832c3dd70a1206e7f4a2a3c65473d5
AzharGhurab <aghurab@innovationteam.com>
2026-04-02 17:26:02 +0300

    Refactor:Added logging for scene casting

--- weatherApp/weatherApp/App/SceneDelegate.swift
+++ weatherApp/weatherApp/App/SceneDelegate.swift
@@ -13,7 +13,14 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        guard let windowScene = (scene as? UIWindowScene) else { return }
+        guard let windowScene = (scene as? UIWindowScene) else {
+            print("""
+              Failed to cast scene to UIWindowScene
+              Scene value: \(scene)
+              Scene type: \(type(of: scene))
+              """)
+            return
+        }
          
         let window = UIWindow(windowScene: windowScene)
                let vc = ViewController(nibName: "ViewController", bundle: nil)
